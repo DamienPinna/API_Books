f61fb6e9d01b42587a3ce075b1b4445d5dba26e8
Damien Pinna <damien.pinna@gmail.com>
2023-05-21 11:57:16 +0200

    ajout du controller BookController

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
 use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -15,5 +16,11 @@ class BookController extends AbstractController {
     $bookList = $bookRepository->findAll();
     $jsonBookList = $serializerInterface->serialize($bookList, 'json');
     return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
+  }
+
+  #[Route('/api/book/{id}', name: 'book', methods: ['GET'])]
+  public function getOneBook(Book $book, SerializerInterface $serializerInterface): JsonResponse {
+    $jsonBook = $serializerInterface->serialize($book, 'json');
+    return new JsonResponse($jsonBook, Response::HTTP_OK, [], true);
   }
 }
