61634298f4c3b63c998965288d810f1cafe8428d
Damien Pinna <damien.pinna@gmail.com>
2023-05-21 16:44:27 +0200

    ajout de l'update d'un book

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 class BookController extends AbstractController {
   #[Route('/api/books', name: 'books', methods: ['GET'])]
@@ -51,5 +52,20 @@ class BookController extends AbstractController {
     $location = $urlGeneratorInterface->generate('book', ['id' => $book->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
 
     return new JsonResponse($jsonBook, Response::HTTP_CREATED, ["Location" => $location], true);
+  }
+
+  #[Route('/api/update/book/{id}', name: 'update-book', methods: ['PUT'])]
+  public function updateOneBook(Book $curentBook, Request $request, SerializerInterface $serializerInterface, EntityManagerInterface $entityManagerInterface, AuthorRepository $authorRepository): JsonResponse {
+    $updateBook = $serializerInterface->deserialize($request->getContent(), Book::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $curentBook]);
+
+    $content = $request->toArray();
+    $idAuthor = $content['idAuthor'] ?? -1;
+
+    $updateBook->setAuthor($authorRepository->find($idAuthor));
+
+    $entityManagerInterface->persist($updateBook);
+    $entityManagerInterface->flush();
+
+    return new JsonResponse(null, Response::HTTP_NO_CONTENT);
   }
 }
